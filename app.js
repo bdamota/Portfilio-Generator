4e2b5b3078d90056782ca18f5640b3676e8102ae
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-15 13:08:26 -0700

    html output modified

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src');
-//...
-
-// const pageHTML = generatePage(portfolioData);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw new Error(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -134,9 +125,13 @@ const promptUser = () => {
       })
   };
 
-  promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-  });
+   promptUser()
+     .then(promptProject)
+     .then(portfolioData => {
+      const pageHTML = generatePage(portfolioData);
 
+      fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+      console.log('Portfolio complete! Check out index.html to see the output!')
+    });
+})
